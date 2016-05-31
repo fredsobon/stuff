16f36d47b628f37e29d7931c6aee1ec91d9c82df
fredsobon <frederic.sobon@gmail.com>
2016-05-31 21:37:34 +0200

    up

--- scripts/bash/node_target.sh
+++ scripts/bash/node_target.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-
 arg="${1}"
 search_file="/etc/hosts"
-filter=$(grep -Ei "${arg}" "${search_file}" |grep -iE ".frontend" |awk '{print $2}')
+filter=$( grep -Ei "${arg}" "${search_file}" |grep  ".frontend" |grep -v "old" |awk '{print $2}')
+
+echo  "here are the target machines : "
+echo "$filter" |sed -n 's/.frontend//p' 
+echo "let's go :"
+ 
+
+cssh $(echo -n "$filter" |sed 's/.frontend//p' |sed ':a;N;$!ba;s/\n/ /g') 
 
 
-echo -n "$filter" |sed 's/.frontend//p'
+
+
+
 
 
 
