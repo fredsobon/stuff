8504d87112437c105fb4c4b56959849b2add0aeb
fredsobon <frederic.sobon@gmail.com>
2016-06-19 19:33:12 +0200

    up

--- scripts/bash/hosts_parser.sh
+++ scripts/bash/hosts_parser.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 ## this script aims to do some test in order to ensure unicity of records in "/etc/hosts" file record.
+host_file="/etc/hosts"
 
-cat /etc/hosts |grep -viE "^#|test|:|eof|\[temp]|temporairesi|spare" |tr '\t' ' ' |tr ' ' '\n' |sed '/^$/d' |sort -g |uniq -c |grep -Ev "      1" |sort -rn
 
+
+result=$(cat ${host_file} |grep -viE "^#|test|:|eof|\[temp]|temporaires|spare" \
+|tr '\t' ' ' |tr ' ' '\n' |sed '/^$/d' \
+|sort -g |uniq -c |grep -Evi "      1|#|switch|san|irac|bessiere|ulis"|sort -rn)
+
+
+if [ -n "$result" ]; then 
+    echo "Some records are present more than on time in your host file. Please check !"
+    echo " $result"
+    exit 1
+else
+    exit 0 
+fi
