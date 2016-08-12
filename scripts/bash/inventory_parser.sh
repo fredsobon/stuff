f970ff46c4706f708c12bf93edc83a32124e5b5d
f.sobon <f.sobon@meetic-corp.com>
2016-08-12 18:54:46 +0200

    up

--- scripts/bash/inventory_parser.sh
+++ scripts/bash/inventory_parser.sh
@@ -6,4 +6,4 @@ record_file="/home/boogie/Documents/work/work_doc/inventory/.csv"
 
 
 
-grep -iE $node $record_file |awk -F"," '{print $9,"Location=>",$1 ,"=rack number=>",$4,"= rack position =>",$5,"= serial number =>" , $12 }'
+grep -iE $node $record_file |awk -F"," '{print tolower($9),"Location=>",$1 ,"=rack number=>",$4,"= rack position =>",$5,"= serial number =>" , $12 }'
