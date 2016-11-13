2cdb2f23b7ab754120b485389f6f79a05e97f8ce
fredsobon <frederic.sobon@gmail.com>
2016-11-13 03:40:39 +0100

    up

--- scripts/bash/bck_docs.sh
+++ scripts/bash/bck_docs.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-
-
 # Main goal : bck /home/boogie/Documents/learn folder and all contents ( which are the main doc to be retrieve) on a nexternal hdd 
-
-
 
 src="/home/boogie/Documents/learn"
 dst="/media/boogie/bck_flash"
 
-
 # test de presence du montage dans proc/mounts : nb pas de "/" a la fin du pattern :
 dst_check="$(grep "${dst}" /proc/mounts 2>&1)"
 res=$?
-
 
 if [ $res -eq 1  ]; then 
 	echo "no way check your moutpoint plz "
@@ -21,13 +15,10 @@ else
 	echo "mount point ok ...." 
 fi  
 
-
 # test du contenu du repertoire ( si vide le retour du test est faux ) :
 [ "$(ls -A $dst)" ] && echo "Not Empty" || echo "Empty"
 
-
 # test :
-
 if [ -f $dst ]; then
 	echo "please ensure your bck device is correctly recognize by the system.."
 	echo "..thx .."
@@ -51,4 +42,3 @@ else
 		
 	fi
 
-	
