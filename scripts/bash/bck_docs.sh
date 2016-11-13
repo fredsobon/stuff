a9047416145f21de9c652eea5aefc5f00370c43b
fredsobon <frederic.sobon@gmail.com>
2016-11-13 03:38:36 +0100

    up

--- scripts/bash/bck_docs.sh
+++ scripts/bash/bck_docs.sh
@@ -11,6 +11,16 @@ dst="/media/boogie/bck_flash"
 
 # test de presence du montage dans proc/mounts : nb pas de "/" a la fin du pattern :
 dst_check="$(grep "${dst}" /proc/mounts 2>&1)"
+res=$?
+
+
+if [ $res -eq 1  ]; then 
+	echo "no way check your moutpoint plz "
+	exit 1 
+else 
+	echo "mount point ok ...." 
+fi  
+
 
 # test du contenu du repertoire ( si vide le retour du test est faux ) :
 [ "$(ls -A $dst)" ] && echo "Not Empty" || echo "Empty"
@@ -18,25 +28,26 @@ dst_check="$(grep "${dst}" /proc/mounts 2>&1)"
 
 # test :
 
-	if [ -f $dst ]; then
-		echo "please ensure your bck device is correctly recognize by the system.."
-		echo "..thx .."..
-	else
-		echo "seems to be cool ..let's start ..."
-		cd $src
-		echo " we're now in $src folder ..."
-	        echo "gonna make a dry run first .."
-		rsync -azvn . $dst
-		read -p "is this the objective ??" ans
-		echo "your answer is $ans .."
-		case "$ans" in 
-			y|yes|Y|Y..) rsync -azv . $dst
-			;;
-			n|no|N|NO) echo "alright ..let 's go out .."
-			;;
-			*) echo "please the answer should be <y|yes|Y|Y..> or n|no|N|NO ..then try again.."
-			;; 
-		esac
+if [ -f $dst ]; then
+	echo "please ensure your bck device is correctly recognize by the system.."
+	echo "..thx .."
+	exit 2
+else
+	echo "seems to be cool ..let's start ..."
+	cd $src
+	echo " we're now in $src folder ..."
+        echo "gonna make a dry run first .."
+	rsync -azvn . $dst
+	read -p "is this the objective ??" ans
+	echo "your answer is $ans .."
+	case "$ans" in 
+		y|yes|Y|Y..) rsync -azv . $dst
+		;;
+		n|no|N|NO) echo "alright ..let 's go out .."
+		;;
+		*) echo "please the answer should be <y|yes|Y|Y..> or n|no|N|NO ..then try again.."
+		;; 
+	esac
 		
 	fi
 
