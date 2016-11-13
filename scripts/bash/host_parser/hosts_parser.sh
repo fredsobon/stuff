57044fac827e0e2398403d13454659b6440f4e74
fredsobon <frederic.sobon@gmail.com>
2016-11-13 03:51:01 +0100

    up

--- scripts/bash/host_parser/hosts_parser.sh
+++ scripts/bash/host_parser/hosts_parser.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 ## this script aims to do some test in order to ensure unicity of records in "/etc/hosts" file record.
-host_file="scripts/bash/host_parser/hosts"
-filter1="scripts/bash/host_parser/filter1"
-filter2="scripts/bash/host_parser/filter2"
+
+host_file="profile/files/hosts"
+precommit_path=$(readlink  $0)
+filter_path=$(dirname "$precommit_path")
+RED='\033[0;31m'
+NC='\033[0m'
 
 ## in order to capitalized pattern to exclude 2 files have been created each one contains text parsed by grep 
 # file number one called filter1 - file number2 called filter2 
 
-result=$(cat ${host_file} |grep -viE --file "$filter1" \
+result=$(sed 's/#.*//' "$host_file" |grep -viE --file "$filter_path"/filter1 \
 |tr '\t' ' ' |tr ' ' '\n' |sed '/^$/d' \
-|sort -g |uniq -c |grep -Evi --file "$filter2" |sort -rn)
+|sort -g |uniq -c |awk '$1 > 1 {print}'|sort -rn)
 
 
 if [ -n "$result" ]; then 
-    echo "Some records are present more than one time in your host file. Please check ! : "
+    echo -e "${RED}Some records are present more than one time in your host file. Please check ! :${NC}"
     echo ""
     echo  "$result" |awk '{print "number => " $1  "    record name => " $2}'
     exit 1
