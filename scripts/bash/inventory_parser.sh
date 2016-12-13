742e7134d889c420e69791ea1f4f29b7903813a6
fredsobon <frederic.sobon@gmail.com>
2016-12-13 20:08:57 +0100

    ip

--- scripts/bash/inventory_parser.sh
+++ scripts/bash/inventory_parser.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-node="$1"
-record_file="/home/boogie/Documents/work/work_doc/inventory/*.csv"
-grep -iE $node $record_file |awk -F"," '{print $9,"Location =>",$1 , "room =>" $2 , "rack_number => " $3 , "= rack_position_front =>",$4, "= rack_position_back =>",$5,"= serial number =>" , $12 , "= IL Id - colt reference => ", $10 }'
 
-#grep -iE $node $record_file |awk -F"," '{print tolower($9),"Location=>",$1 ,"=rack number=>",$4,"= rack position =>",$5,"= serial number =>" , $12 }'
+usage() {
+cat <<EOF
+NAME
+        $(basename $0) - inventory parser 
+
+SYNOPSIS
+        Usage: $(basename $0) <HOST> [<HOSTN> ....]
+
+DESCRIPTION
+        Query the csv file extracted from the original xls in order to retrieve nodes informations
+
+        The csv file format is mandatory  this have to be exported from the original excel. (tools are available)
+                                
+EOF
+}
+
+[ $# -lt 1 ] && {
+        echo "Missing argument."
+        usage
+        exit 1
+}
+
+node="$1"
+record_file="*.csv"
+real_path=$(readlink -f $record_file)
+echo "===== File parsed :  $real_path ==="
+
+for i in $@
+do
+	grep -iE $node $real_path |awk -F"," '{print tolower($9),"Location =>",$1 , "room =>" $2 , "rack_number => " $3 , "= rack_position_front =>",$4, "= rack_position_back =>",$5,"= serial number =>" , $12 , "= IL Id - colt reference => ", $10 }'
+
+done
+shift 1
