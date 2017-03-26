72fa86a882f7c5367f6f5b10880e2de9b468f24d
fredsobon <frederic.sobon@gmail.com>
2017-03-26 07:31:18 +0200

    up

--- buffer/server_move.sh
+++ buffer/server_move.sh
@@ -4,48 +4,65 @@
 
 #func :
 
-server_out () {
-echo "$node selected "  
-sed "s/\""$node"\",//g" $file >> prod.new
+server_out_chk () {
+      grep "$node" "$file"
+      chk="$?"
+      if [ _"$chk" != "_0" ]; then 
+          echo "no way the node is not present !" 
+          exit 2
+      fi
 }
 
-server_def () {
+server_in_chk () {
       grep "$node" "$file"
       chk="$?"
       if [ _"$chk" = "_0" ]; then 
           echo "no way the node is already present !" 
           exit 3
-      else 
+      fi
+}
+
+server_out () {
+echo "$node selected "  
+sed "s/,*\""$node"\",*//g" $file >>  yop
+}
+
+server_def () {
          echo "all right you would like to add a node in $nodedef role categorie ..a kind already exists in $file"
          grep -in $nodedef $file
          line=$(grep -in $nodedef $file |awk -F: '{print $1}')
-      fi
 }
 
 server_in () {
 echo "$node to be added in $file at the end of line : $line"
-echo "sed  "$line" "s/\(.*$\)/\1, \""$node"\"/" t"
-#sed -i ""$line" s/\(.*$\)/\1, \""$node"\"/" t
-sed -i "s/\(.*$\)/\1, \""$node"\"/" t
+sed -i "s/\(.*$\)/\1, \""$node"\"/" $file
 
 }
 
 # var :
 file="prod.rb"
+cp ${file} ${file}.ori
 
 
-read -p "gimme a node : " node
+echo "################"
 read -p "action on node : add or remove ?" action
-
+echo "################"
 
 
 case $action in 
 
-add) read -p "please provide the plateform and the role of the server to be added. aka for instance : mwebfront, xwebapipriv and so ..." nodedef
-         server_def   
-         server_in
+
+add)
+read -p "gimme a node : " node
+server_in_chk
+read -p "please provide the plateform and the role of the server to be added. aka for instance : mwebfront, xwebapipriv and so ..." nodedef
+server_def   
+server_in
 ;;
-remove) server_out
+remove)
+read -p "gimme a node to be deleted: " node
+server_out_chk
+server_out
 ;;
 *) echo " please choose between <add> or <remove> ..else ciao!"
 esac
