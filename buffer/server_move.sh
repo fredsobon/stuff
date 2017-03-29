ba12a4827d644036aed9cdc3a9639da032f8dbdb
fredsobon <frederic.sobon@gmail.com>
2017-03-29 20:23:51 +0200

    up

--- buffer/server_move.sh
+++ buffer/server_move.sh
@@ -4,14 +4,57 @@
 
 #func :
 
+
+# node remove : 
+
 server_out_chk () {
+
+      ## check node presence before deleting : 
+
       grep "$node" "$file"
       chk="$?"
       if [ _"$chk" != "_0" ]; then 
           echo "no way the node is not present !" 
-          exit 2
+          exit 1
+      fi
+
+	  # check position in file : in order to establish the dedicated text action to do.
+      
+      # begining : grep -E   "[[:alnum:]]*, \"${node}\"," prod.rb
+      grep -Eq   "[[:alnum:]]*, \"${node}\"," $file
+      chk="$?"
+      if [ _"$chk" = "_0" ]; then
+      echo "allright $node is the first of the list in the same category" 
+      pattern="\"${node}\","
+	  sed -i "s#${pattern}##" $file
+      fi
+	  
+	  # node between some other server of the category :
+      grep -Eq   "\"[[:alnum:]]*, \"${node}\"," "$file"
+      chk="$?"
+      if [ _"$chk" = "_0" ]; then
+      echo "allright $node is one between some of the same category"
+      pattern="\"${node}\","
+	  sed -i "s#${pattern}##" $file
+      fi
+
+      # last one of the category : 
+      grep -Eq   "\"[[:alnum:]]*, \"${node}\"$" "$file"
+      chk="$?"
+      if [ _"$chk" = "_0" ]; then
+      echo "allright $node is the last of the  category" 
+      pattern=", \"${node}\""
+	  sed -i "s#${pattern}##" $file
       fi
 }
+
+server_out () {
+echo "$node selected "
+sed -i "s#,*\""$node"\",*##g" $file
+}
+
+
+## node injection : 
 
 server_in_chk () {
       grep "$node" "$file"
@@ -22,20 +65,22 @@ server_in_chk () {
       fi
 }
 
-server_out () {
-echo "$node selected "  
-sed "s/,*\""$node"\",*//g" $file >>  yop
-}
-
 server_def () {
-         echo "all right you would like to add a node in $nodedef role categorie ..a kind already exists in $file"
-         grep -in $nodedef $file
+      echo "all right you would like to add a node in $nodedef role categorie ..a kind already exists in $file"
+      grep -in $nodedef $file
+      chk="$?"
+      if [ _"$chk" = "_0" ]; then 
+          echo "no way : you want to inject a node that seems to be unique ..please check the role in the file !" 
+          exit 3
+      fi
+         
          line=$(grep -in $nodedef $file |awk -F: '{print $1}')
 }
 
 server_in () {
-echo "$node to be added in $file at the end of line : $line"
-sed -i "s/\(.*$\)/\1, \""$node"\"/" $file
+echo "< $node > to be added in < $file >  at the end of line : < $line > "
+echo "sed -i ""$line" s#\(.*[[:alnum:]]\"$\)#\1, \""$node"\"#" $file" 
+sed -i ""${line}" s#\(.*[[:alnum:]]\"$\)#\1, \""$node"\"#" $file 
 
 }
 
@@ -44,9 +89,11 @@ file="prod.rb"
 cp ${file} ${file}.ori
 
 
-echo "################"
+echo "################################"
+
 read -p "action on node : add or remove ?" action
-echo "################"
+
+echo "################################"
 
 
 case $action in 
@@ -60,16 +107,11 @@ server_def
 server_in
 ;;
 remove)
+
 read -p "gimme a node to be deleted: " node
 server_out_chk
 server_out
 ;;
-*) echo " please choose between <add> or <remove> ..else ciao!"
+*) echo "Please enter the action you'd like to be done < add > a new node in '.rb' files or < remove > a exiting one from the file(s)..."
+
 esac
-
-## histo cmds : 
-#sed -r 's#[[:alnum:]]{1,}, ([a-z]{1,}[0-9]{1})#\1#' lapin
-# i=photostc5
-#sed "s/\(.*$\)/\1, \""$i"\"/" t
-#role :static, "photostc1",  "photostc3", "photostc4", "photostc5"
-
