2abd6fd5d3f1ba7d022f8b5733f3df78c4998c63
fredsobon <frederic.sobon@gmail.com>
2017-04-05 18:05:02 +0200

    up

--- buffer/sorted.sh
+++ buffer/sorted.sh
@@ -9,13 +9,25 @@ fixed="role :[[:alnum:]]+, "
 
 # on extrait le num de ligne comprenant notre role et la liste de servers :
 grep -En "$fixed" prod1 |cut -d ":" -f 1 >line_file
-echo $fixed
 
 for i in $(cat line_file); do cat prod1 |sed -rn "$i s/$fixed//p" > test_$i  ;done  
 
 
 for piece in $(ls test_$i); do 
-cat "test_$i" |sed 's/[",]//g' |tr ' ' '\n' |sort |tr '\n' ' ' |sed -r "s/([[:alnum:]]+)/\"\1\"/g" |sed -r "s/(\"[[:alnum:]]+\")/\1, /g" |sed -r "s/^(.*)$/"$fixed" \1/g" >> "sort_$i"
+# on reset nos var :
+
+fixed="role :[[:alnum:]]+, "
+grep -Eo "$fixed" prod1|head -1
+res=$(grep -Eo "$fixed" prod1|head -1)
+echo "in loop "$res" ..."
+
+# on delete les " et , ; 
+# on remplace les espaces par des retours chariots  
+# on trie 
+#on remplace les retours chariot par des espaces ; on rajoute les  " 
+# on rajoute les , apres les nodes entourés de " 
+# on rajoute notre role en debut de fichier :
+cat "test_$i" |sed 's/[",]//g' |tr ' ' '\n' |sort |tr '\n' ' ' |sed -r "s/([[:alnum:]]+)/\"\1\"/g" |sed -r "s/(\"[[:alnum:]]+\")/\1, /g" |sed -r "s/^(.*)$/$res\1/g" >> "sort_$i"
 done
 
 
