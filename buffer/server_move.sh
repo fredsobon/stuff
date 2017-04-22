8a296c83e634bce730a041698d6c9770efc214d1
fredsobon <frederic.sobon@gmail.com>
2017-04-22 23:51:41 +0200

    up

--- buffer/server_move.sh
+++ buffer/server_move.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/env bash
 
+##
 ## main goal : eases the record of server in files used in code deployment
+##
 
 ##func :
 
@@ -8,47 +10,37 @@
 
 server_out_chk () {
 
-	  # check position in file : in order to establish the dedicated text action to do.
-      
-      # begining : grep -E   "[[:alnum:]]*, \"${node}\"," prod.rb
+# check position in file : in order to establish the dedicated text action to do.
 for file in $(cat inputlist); do 
    if 
        grep -Eq   "[a-z]+, \"${node}\"," $file 
    then
       pattern="\"${node}\","
-	  echo "<$pattern>"
-      echo "$node located at the start of $file "
+      #echo "$node located at the begining of $file "
 	      sed -i "s# ${pattern}##" $file
    elif 	  
        grep -Eq   "\", \"${node}\"," "$file" 
     then 
       pattern="\"${node}\","
-	  echo "<$pattern>"
-      echo "$node is located between other nodes in $file"
+      #echo "$node is located between other nodes in $file"
 	      sed -i "s# ${pattern}##" $file
-    elif
-      # last one of the category : 
-       grep -Eq   "\", \"${node}\"$" "$file" 
-    then
-      pattern=", \"${node}\""
-	  echo "<$pattern>"
-      echo "$node is located at the end of the file $file"
-	      sed -i "s/${pattern}//g" $file
     else
-      ## check node presence before deleting : 
-      echo " in node check ..in $file "
-       grep "$node" "$file" || echo "no way the node is not present !" 
+      # last one of the category : 
+      grep -Eq   "\", \"${node}\"$" "$file" 
+      pattern=", \"${node}\""
+      #echo "$node is located at the end of the file $file"
+	      sed -i "s/${pattern}//g" $file
     fi 
 done
 }
 
-## node injection : 
+# node injection : 
 
 server_in_chk () {
       
 for file in $(cat inputlist); do 
       # just check that the node is not already present : 
-      grep "$node" "$file"
+      grep -q "$node" "$file"
       if [ $? -eq 0 ]; then 
           echo "no way the node is already present in $file !" 
 		  exit 3
@@ -56,59 +48,52 @@ for file in $(cat inputlist); do
       
       # ensure that the role of the server is already defined AND that the first node of this role has to be record in a manual action : safe operation
       role=${node:0:5}
-      grep "$role" "$file"
+      grep -q "$role" "$file"
       if [ $? -ne 0 ]; then 
           echo "be careful the node seems to be the first one of the category. manual check and record needed ...in $file" 
           exit 4
       else
-          line=$(grep -in "$role" "$file" |awk -F: '{print $1}')
-          echo "all right : < $node >  gonna be added in "$file" at line n° "$line" "
+            line=$(grep -En "$role" "$file" |awk -F: '{print $1}')
+            echo "ok : "$node" gonna be added  line number "$line" in "$file" "
+            sed -ri ""${line}" s#(.*[[:alnum:]]\"$)#\1, \""$node"\"#" "$file" 
       fi 
 done
 }
 
-server_in () {
-for file in $(cat inputlist); do 
 
-sed -i ""${line}" s#\(.*[[:alnum:]]\"$\)#\1, \""$node"\"#" $file 
-done
-}
+##
+
 sort_in () {
 
-# retrieve role for nodes 
-grep -Eon "role :[[:alnum:]]+" $file |awk -F: '{print $1}'> line_number
-
-# fake name prefixing role to prepare sort 
-for num in $(cat line_number)
-do
-sed -rn "$num s/role (:[[:alnum:]]+),/1role\1/p" $file > part_$num
-done
-
-
-# delete unused caracters : 
-for i in $(ls part_*)
-do
-       sed -re "s/[\",]//g" $i |sed 's/ /\n/g' |sort |tr '\n' ' ' |sed -re "s/([[:alnum:]]+)/\"\1\"/g" |sed -re "s/(\"[[:alnum:]]+\")/\1,/g"  >> sorted_$i
-done
-
-# last change to clean role and delete unmandatory caracters : 
-
-for p in $(ls sorted_*)
-do
-    sed -rn "s/\"1role\",:\"([[:alnum:]]+)\",/role :\1,/p" $p |sed -rn "s/(.*+\"),/\1/p" >> last_$p
-done
-
-# let's merge it all ...then cleanup ! 
-
-
-awk '{print $0}' last_* >> filefinal
-rm last_* sorted_* part_* line_number 
-mv filefinal $file
+	for dir in $(dirname $(realpath $(cat inputlist)))
+    do 
+        cd "$dir"
+        for file in *
+        do
+            grep -En role "$file" |awk -F: '{print $1}' > line_number
+            sed -ri "s/role (:[[:alnum:]]+),/1role\1/g" "$file"
+            sed -ri "s/[\",]//g" "$file" 
+            for num in $(cat line_number)
+			  do 
+				sed -n "${num}p" $file  > ${file}${num}
+                sed -ri "s/ /\n/g" "${file}${num}"
+			    cat "${file}${num}" |sort |tr '\n' ' ' > "${file}${num}_sort"
+			    sed -ri "s/([[:alnum:]]+)/\"\1\"/g" "${file}${num}_sort"
+			    sed -ri "s/(\"[[:alnum:]]+\")/\1,/g" "${file}${num}_sort"
+		        sed -ri "s/\"1role\",:\"([[:alnum:]]+)\",/role :\1,/" "${file}${num}_sort" 
+			    sed -ri "s/(.*+\"),/\1/" "${file}${num}_sort"
+				awk "{print }" "${file}${num}_sort" >> "${file}_final"
+                rm  "${file}${num}" "${file}${num}_sort"
+                cp "${file}_final" ../buffer/
+		    done
+				rm line_number
+                mv "${file}_final" "$file"
+        done
+    done 
 }
 
+
 ####
-# file prep : delete unecessary space .
-sed -ir 's/\(.*"\)[[:space:]]*/\1/g' $(cat inputlist)
 
 echo "################################"
 
@@ -120,13 +105,42 @@ case $action in
 
 add)
 read -p "gimme a node : " node
+
+# retrieve target files to be process and test if this kind of server is already present ; if not a manual record is better (aka more safe) for the first time ... 
+role=${node:0:5}
+for file in fold/*
+do 
+    grep -q "$role" $file
+    if [ $? -ne 0 ]; then 
+        echo "be careful the node seems to be the first one of the category. manual check and record needed ...in $file" 
+        exit 4
+    fi
+done
+
+grep -l "${node:0:5}" fold/* > inputlist
+echo "Here are the target file(s) :"
+echo "$(cat -n inputlist)"
+# delete potential dirty space at the end of lines : in order to process the job correctly
+for file in $(cat inputlist)
+do
+    sed -i "s#\(.*\"\)[[:space:]]*#\1#g" "$file"
+done
+
 server_in_chk
-server_in
 sort_in
 ;;
 
 remove)
 read -p "gimme a node to be deleted: " node
+
+# retrieve target files to be process 
+grep  -q ^${node}$ "$file" 2>/dev/null || echo "no way the node is not present in $file!" 
+grep -l "$node" fold/* > inputlist
+# delete potential dirty space at the end of lines : in order to process the job correctly
+for file in $(cat inputlist)
+do
+    sed -i "s#\(.*\"\)[[:space:]]*#\1#g" $file
+done
 server_out_chk
 ;;
 
