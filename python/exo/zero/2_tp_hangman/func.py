e52f1346576ebe6ca548e39ed57a4587688e8f5e
fredsobon <f.sobon@meetic-corp.com>
2017-11-25 16:37:54 +0100

    up

--- python/exo/zero/2_tp_hangman/func.py
+++ python/exo/zero/2_tp_hangman/func.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-import random
+import random , pickle 
 
-
+### main functions file. each one is being tested at the end in a dedicated section.
 ## define player : 
 
 
-## define secret word :
-
+# get the word list and write it to a file called data.py in the current dir  
 liste = ["lapin", "coco" ]
+file = "data.py"
+
+def dump_list(file):
+    with open("data.py", "wb") as file:
+        send_data = pickle.Pickler(file)
+        send_data.dump(liste)
+        
+# retrieve the word liste from the data file and then get the secret word using random func :
+
+def get_list(file):
+    with open("data.py", "rb") as file:
+        get_data = pickle.Unpickler(file)
+        get = get_data.load()
+        print(">>>", get ,"<<<")
+# check data file content : 
 # got to define a number (equal to the lenght of list minus 1 : to be in a correct range :
 word_index = random.randint(0,len(liste)) - 1
 word = liste[word_index]
 def test(word):
-    if len(word) > 8:
-        print("no way the selected word is too long  !")
-    else:
+    try:
+        assert 0 < len(word) < 8
         print("ok the secret word is correct and is : "  , word.upper() )
-test(word)       
-
-
+    except AssertionError: 
+        print("no way the given word is over string limit !")  
+        exit()
 ## retrieve  the letter given by the player : 
 letter = input("give us a letter:" )
 print("ok you choose :", letter)
@@ -27,8 +40,6 @@ print("ok you choose :", letter)
 def fonc(letter):
     print("let us try to find if", letter ," is in the secret word !!")
     return
-
-fonc(letter) 
 
 ## let's printout choosen letter in secret word if present :
 
@@ -39,8 +50,10 @@ def game():
         else:
             print("*")
     return word
-game()
 
-
-
-
+## test func inside the module itself :
+if __name__ == "__main__":
+    get_list(file)
+    test(word)       
+    fonc(letter) 
+    game()
