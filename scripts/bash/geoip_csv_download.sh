54f0c4a5c4b0b30bd09615df4ebfe28c2c91e2ac
f.sobon <f.sobon@meetic-corp.com>
2018-10-17 22:30:32 +0200

    up

--- scripts/bash/geoip_csv_download.sh
+++ scripts/bash/geoip_csv_download.sh
@@ -3,14 +3,15 @@
 path_csv="/home/boogie/Bureau/run/geoip_csv/test/"
 file_type="GeoIP2-City-CSV GeoIP2-ISP-CSV GeoIP2-Anonymous-IP-CSV"
 bck_folder="/tmp/"
+key="blabla"
 date=$(date +%F)
 
 for file in $(echo ${file_type})
 do 
   # download zip files :
-  curl -s -o ${file}.zip https://download.maxmind.com/app/geoip_download?edition_id=${file}\&license_key=si66vuh7Wp6w\&suffix=zip
+  curl -s -o ${file}.zip https://download.maxmind.com/app/geoip_download?edition_id=${file}\&license_key=${key}\&suffix=zip
   # download md5 files :
-  curl -s -o ${file}.md5 https://download.maxmind.com/app/geoip_download?edition_id=${file}\&license_key=si66vuh7Wp6w\&suffix=zip.md5
+  curl -s -o ${file}.md5 https://download.maxmind.com/app/geoip_download?edition_id=${file}\&license_key=${key}\&suffix=zip.md5
 
   # compare checksum : 
   md5=$(md5sum ${file}.zip |awk '{print $1}')
@@ -19,7 +20,7 @@ do
     echo "${file} downloaded the $date is correct" >> ${date}_log
   else 
     echo "file corruption!"  
-    curl -s -o ${file}.zip https://download.maxmind.com/app/geoip_download?edition_id=${file}\&license_key=si66vuh7Wp6w\&suffix=zip
+    curl -s -o ${file}.zip https://download.maxmind.com/app/geoip_download?edition_id=${file}\&license_key=${key}\&suffix=zip
   fi
 done
 
