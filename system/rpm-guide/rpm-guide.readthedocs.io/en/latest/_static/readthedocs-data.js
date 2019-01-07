c0031b6cebc464a2a8583726ead71516fdda800b
f.sobon <f.sobon@meetic-corp.com>
2019-01-07 16:03:41 +0100

    up

---
+++ system/rpm-guide/rpm-guide.readthedocs.io/en/latest/_static/readthedocs-data.js
@@ -0,0 +1,14 @@
+  var READTHEDOCS_DATA = {
+    project: "rpm-guide",
+    version: "latest",
+    language: "en",
+    subprojects: {},
+    canonical_url: "http://rpm-guide.readthedocs.io/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/source/",
+    source_suffix: ".rst",
+    api_host: "https://readthedocs.org",
+    commit: "d6e0fa2c"
+  };
+  
