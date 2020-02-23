576f7270433543dfd3e6f8d3235791b0ee1d8c46
boogie <jazz77777@gmail.com>
2020-02-23 11:44:42 +0100

    up

---
+++ scripts/bash/desktop_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y screen tmux tree tcpdump wireshark nmap lsof strace net-tools gnupg meld xlsx2csv hfsplus hfsprogs hfsutils terminator curl wget tshark keepassx typora  remmina visualvm vim gnome-tweak-tool
+
+
+sudo mkdir -p /home/boogie/Documents/{learn, own, work, stuff}
+
+
