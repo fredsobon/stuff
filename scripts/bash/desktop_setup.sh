fbf01604c7c68e0444fbaab220785d95c2a01ef0
boogie <f.sobon@meetic-corp.com>
2020-02-23 13:18:30 +0100

    up

--- scripts/bash/desktop_setup.sh
+++ scripts/bash/desktop_setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y screen tmux tree tcpdump wireshark nmap lsof strace net-tools gnupg meld xlsx2csv hfsplus hfsprogs hfsutils terminator curl wget tshark keepassx typora  remmina visualvm vim gnome-tweak-tool
+sudo apt install -y screen tmux tree tcpdump wireshark nmap lsof strace net-tools gnupg meld xlsx2csv hfsplus hfsprogs hfsutils terminator curl wget tshark keepassx  remmina visualvm vim gnome-tweak-toola git
 
 
-sudo mkdir -p /home/boogie/Documents/{learn, own, work, stuff}
+sudo mkdir -p /home/boogie/Documents/{learn,own,work}
 
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install google-chrome-stable
