7615244ea318031c4e3467cc4aafba75aca805fe
fredsobon <f.sobon@meetic-corp.com>
2020-02-23 14:57:40 +0100

    up

--- scripts/bash/desktop_setup.sh
+++ scripts/bash/desktop_setup.sh
@@ -16,3 +16,40 @@ sudo apt install libgl1-mesa-glx libxcb-xtest0
 then dpkg -I zoom pck dl from their website
 
         
+## kube section :
+
+
+# minikube set up - using kvm 
+
+sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
+  && chmod +x minikube
+sudo cp minikube /usr/local/bin && rm minikube
+minikube config set vm-driver kvm2
+
+# kubectl binary : 
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# k9s :
+curl -LO https://github.com/derailed/k9s/releases/download/v0.16.1/k9s_Linux_x86_64.tar.gz
+tar -xzvf k9s_Linux_x86_64.tar.gz
+sudo mv k9s /usr/local/bin
+
+
+# krew set up : kube plugin manager :
+
+(
+  set -x; cd "$(mktemp -d)" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/download/v0.3.4/krew.{tar.gz,yaml}" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_amd64" &&
+  "$KREW" install --manifest=krew.yaml --archive=krew.tar.gz &&
+  "$KREW" update
+)
+
+# set up ctx and ns plugins :
+kubectl krew install ctx
+kubectl krew install ns
