53c66f9e755300436bdf941af6513a4a705a5f1a
fredsobon <f.sobon@meetic-corp.com>
2020-04-22 11:26:09 +0200

    up

--- scripts/bash/desktop_setup.sh
+++ scripts/bash/desktop_setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 sudo apt update -y && sudo apt upgrade -y
-sudo apt install -y screen tmux tree tcpdump wireshark nmap lsof strace net-tools gnupg meld xlsx2csv hfsplus hfsprogs hfsutils terminator curl wget tshark keepassx  remmina visualvm vim gnome-tweak-toola git
+sudo apt install -y screen tmux tree tcpdump wireshark nmap lsof strace net-tools gnupg meld xlsx2csv hfsplus hfsprogs hfsutils terminator curl wget tshark keepassx  remmina visualvm vim gnome-tweak-toola git docker.io
 
+sudo addgroup boogie docker
 
-sudo mkdir -p /home/boogie/Documents/{learn,own,work}
+sudo mkdir -p /home/boogie/Documents/{learn,own,work,lab}
 
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -13,7 +14,7 @@ sudo apt-get install google-chrome-stable
 
 ## set up dependencies for zoom app (conf call and video ) :
 sudo apt install libgl1-mesa-glx libxcb-xtest0
-then dpkg -I zoom pck dl from their website
+#then dpkg -I zoom pck dl from their website
 
         
 ## kube section :
